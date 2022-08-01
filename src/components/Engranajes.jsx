72a0a1fe7964aeadda72859f782fc832b425abf5
Alejandro Yarce <53787660+DonYarce@users.noreply.github.com>
2022-08-01 11:56:19 -0500

    Update Engranaje
    Update engranaje

--- src/components/Engranajes.jsx
+++ src/components/Engranajes.jsx
@@ -1,6 +1,6 @@
 const Engranajes = ({ idioma, eng1, eng2, eng3, eng4, eng5, eng6 }) => (
   <div className='flex justify-evenly pt-6 w-full'>
-    <p className='text-white text-2xl pt-2 font-bold w-24 '>{idioma}</p>
+    <p className='text-white text-xl pt-1 font-bold w-24 '>{idioma}</p>
     <div className='flex'>
       <img width='40' src={eng1} alt='foto' />
       <img width='40' src={eng2} alt='foto' />
